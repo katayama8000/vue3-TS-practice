4cab53b43c312e8d396abd28da9b0c51db7e81be
katayama <tattu.0310@gmail.com>
2022-09-21 19:03:50 +0900

    ✨ feat/vuex logic

--- src/page/Test.vue
+++ src/page/Test.vue
@@ -2,26 +2,97 @@
 import { computed } from "@vue/reactivity";
 import { useStore } from "vuex";
 const store = useStore();
+
 //const count = store.state.count;
 const { count, arrayName } = useStore().state;
-const onIncrement = () => {
-  store.commit("increment");
+
+//increment
+const onIncrement = (num: number): void => {
+  store.commit("increment", num);
+};
+
+//decrement
+const onDecrement = (num: number): void => {
+  store.commit("decrement", num);
+};
+
+//addName
+const addName = (): void => {
+  store.commit("addName", "test");
 };
 
 //storeの更新を検知する
-const detectedCount = computed(() => {
+const detectedCount = computed<number>(() => {
   return useStore().state.count;
 });
+
+//storeの更新を検知する
+const detectedArrayName = computed<string[]>(() => {
+  return useStore().state.arrayName;
+});
+
+//leftItems
+const leftItems = computed<number>(() => {
+  return useStore().state.leftItems;
+});
+
+//rightItems
+const rightItems = computed<number>(() => {
+  return useStore().state.rightItems;
+});
+
+//moveLeft
+const moveLeft = (): void => {
+  store.commit("moveItem");
+};
+
+//moveRight
+const moveRight = (): void => {
+  store.commit("moveItem2");
+};
+
+const moveLeftToRight = (): void => {
+  store.commit("moveLeftToRight");
+};
+
+const moveRightToLeft = (): void => {
+  store.commit("moveRightToLeft");
+};
 </script>
 
 <template>
-  <button @click="onIncrement">incrementcount</button>
+  <button @click="onIncrement(2)" class="bg-red-200 p-2 m-1">
+    incrementcount
+  </button>
+  <button @click="onDecrement(5)" class="bg-blue-200 p-2 m-1">decrement</button>
+
   <div>カウント: {{ count }}/{{ detectedCount }}</div>
+  <button @click="addName">addName</button>
+  <div>名前: {{ arrayName }}/{{ detectedArrayName }}</div>
   <div>
     <ul>
       <li v-for="item in arrayName" :key="item.id">
         {{ item }}
       </li>
     </ul>
+  </div>
+  <hr />
+  <div class="flex">
+    <div class="w-1/2">
+      <h1>left</h1>
+      <div v-for="item in leftItems" :key="item">
+        {{ item }}
+      </div>
+      <button @click="moveRight" class="bg-green-200">右に移動</button>
+      <button @click="moveLeftToRight" class="bg-purple-200">右に移動</button>
+    </div>
+    <div class="w-1/2">
+      <h1>right</h1>
+      <div v-for="item in rightItems" :key="item">
+        {{ item }}
+      </div>
+      <button @click="moveLeft" class="bg-green-200">左に移動</button>
+      <button @click="moveRightToLeft" class="bg-purple-200">左に移動</button>
+    </div>
   </div>
 </template>
--- src/store/index.ts
+++ src/store/index.ts
@@ -4,15 +4,56 @@ export default createStore({
   state: {
     count: 0,
     arrayName: ["tom", "jerry", "jack"],
+    leftItems: ["apple", "banana", "orange"],
+    rightItems: ["grape", "pear", "watermelon"],
   },
   getters: {
     //count
-    count: (state) => state.count,
+    //count: (state) => state.count,
+    //arrayName
+    //arrayName: (state) => state.arrayName,
   },
   mutations: {
-    increment(state) {
-      state.count++;
+    increment(state, number) {
+      state.count += number;
       console.log(state.count);
+    },
+    decrement(state, number) {
+      state.count -= number;
+      console.log(state.count);
+    },
+    addName(state, name) {
+      state.arrayName.push(name);
+      //[...state.arrayName, name];
+      console.log(state.arrayName);
+    },
+    //leftItemsの最後の要素をrightItemsに移動
+    moveItem(state) {
+      const item = state.leftItems.pop();
+      state.rightItems.push(item!);
+    },
+    //RightItemsの最後の要素をleftItemsに移動
+    moveItem2(state) {
+      const item = state.rightItems.pop();
+      state.leftItems.push(item!);
+    },
+    moveLeftToRight(state) {
+      if (state.leftItems.length === 0) {
+        alert("左側にアイテムがありません");
+      }
+      const item: string = state.leftItems.slice(-1)[0];
+      state.rightItems = [...state.rightItems, item];
+      state.leftItems = state.leftItems.slice(0, -1);
+    },
+
+    moveRightToLeft(state) {
+      if (state.rightItems.length === 0) {
+        alert("右のリストに要素がありません");
+      }
+      const item: string = state.rightItems.slice(-1)[0];
+      state.leftItems = [...state.leftItems, item];
+      state.rightItems = state.rightItems.slice(0, -1);
+      console.log(state.rightItems);
     },
   },
   actions: {},
