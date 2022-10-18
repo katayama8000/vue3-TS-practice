3f0555f185be6eadd57d08f6e7a2e3efa8099fa2
katayama <tattu.0310@gmail.com>
2022-10-18 17:40:54 +0900

    v-on

---
+++ src/page/Bind.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const youTubeUrl = "https://www.youtube.com";
+const attribute = "href";
+const twitterUrl = "https://twitter.com";
+const number = ref(0);
+</script>
+
+<template>
+  <h1>bind</h1>
+  <a v-bind:[attribute]="youTubeUrl">YouTube</a>
+  // aタグにオブジェクト付けられない？
+  <a v-bind="{ href: twitterUrl }">Twitter</a>
+  <span>{{ number }}</span>
+  <button v-on:click="number += 1">+</button>
+</template>
