8fbffe6119e7234c843835349b97b6ab76852621
katayama8000 <tattu.0310@gmail.com>
2022-12-30 12:07:31 +0900

    update

--- src/page/Sample7.vue
+++ src/page/Sample7.vue
@@ -2,6 +2,7 @@
 import { ref } from "vue";
 const isLogin = ref<boolean>(false);
 const loginword = `ログイン中`;
+const html = `<p>ログイン中</p>`;
 </script>
 
 <template>
@@ -10,5 +11,8 @@ const loginword = `ログイン中`;
   </button>
   <hr />
   {{ isLogin ? loginword : null }}
+  {{ isLogin && "ログイン中" }}
+  {{ false && "hello" }}
   <p v-bind:style="{ display: isLogin ? '' : 'none' }">ログイン中</p>
+  <template v-html="html"></template>
 </template>
